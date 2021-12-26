850aac994342e1e24061482726a11b604326f997
jriosfer <47304131+jriosfer@users.noreply.github.com>
2021-12-26 17:51:41 -0500

    added creation of the dictionary with heights and loop that looking for other players with the height difference
    added creation of the dictionary with heights and loop that looking for other players with the height difference

--- getPairs.py
+++ getPairs.py
@@ -1,5 +1,6 @@
 import requests
-import json
+from collections import defaultdict
+
 # loadData: takes a URL and headers parameters for getting info from the URL endpoint
 # parameter URL: this is the endpoint where the data is located in JSON format
 # parameter headers: There is no need of headers for accessing this data, so it will be empty
@@ -18,6 +19,27 @@ def getInput():
         except ValueError:
             print("Please introduce a number")
      return result
+
+# parameters:
+#  data: Dictionary with players Info
+#  totalHeight: the total height of the players' pair
+def createHeightDictionary(data, totalHeight):
+    # create an empty dictionary with heights
+    dictPairs = defaultdict(list)
+    # check if
+    print(type(dictPairs))
+    for player in data:
+        try:
+            # takes playerHeight of each player record and validates data quality
+            playerHeight = int(player['h_in'])
+        except ValueError:
+            print("Height is not an integer value on player:" + player['first_name'] + " " + player['last_name'])
+
+        # add player to the dictionary
+        dictPairs[playerHeight].append(player['first_name'] + " " + player['last_name'])
+        # print(dictPairs)
+
+    return dictPairs
 # program starting point
 totalHeight = getInput()
     
@@ -28,3 +50,17 @@ Dataresult=loadData(url,{})
 print(type(Dataresult))
 for data in Dataresult['values'] :
     print(data)		
+
+playersHeight = createHeightDictionary(Dataresult['values'],totalHeight)
+print(playersHeight)
+# search pairs depends on each heigh against another player
+print("The pairs of basketball players that added up:["+ str(totalHeight)+"] inches are:")
+for height in playersHeight:
+    # print(height)
+    if totalHeight - height in playersHeight:
+      for otherPlayer in playersHeight[totalHeight - height] :
+          for player in playersHeight[height]:
+                 #print("player:"+player)
+                 if player < otherPlayer:
+                       print("["+player + " - " + otherPlayer+"]")
+                       
